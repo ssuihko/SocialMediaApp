0f04ca8369ca868ad19dfcc316afcfd2d460b3dc
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-03-18 09:31:50 +0100

    preliminary header and sidebar

--- SoMe_frontend/src/App.jsx
+++ SoMe_frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import { createContext, useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+export const AppContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <AppContext.Provider value={{ posts, setPosts }}>
+      <header className="header">Social-Media App</header>
+      <div className="container">
+        <div className="sidebar">
+          <button>Home</button>
+          <button>Profile</button>
+        </div>
+        <div className="body">Posts</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </AppContext.Provider>
+  );
 }
 
-export default App
+export default App;
