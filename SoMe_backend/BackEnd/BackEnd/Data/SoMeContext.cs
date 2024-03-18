5d9e02ecb37689d7c6055e5e8d0d500ccbf36dd2
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-03-18 11:18:56 +0100

    data migration skeleton complete

---
+++ SoMe_backend/BackEnd/BackEnd/Data/SoMeContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+
+namespace BackEnd.Data
+{
+    public class SoMeContext : DbContext
+    {
+        private string _connectionString;
+        public SoMeContext(DbContextOptions<SoMeContext> options) : base(options)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
+            this.Database.EnsureCreated();
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+            optionsBuilder.LogTo(message => Debug.WriteLine(message));
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        
+    }
+}
---
+++ SoMe_backend/BackEnd/BackEnd/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
+
+namespace BackEnd.Models
+{
+    [Table("posts")]
+    public class Post
+    {
+        [Column("post_id")]
+        public int postId { get; set; }
+        [Column("user_id")]
+        public int userId { get; set; }
+        [Column("title")]
+        public string title { get; set; }
+        [Column("content")]
+        public string content { get; set; }
+        [Column("likes")]
+        public int likes { get; set; }
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
