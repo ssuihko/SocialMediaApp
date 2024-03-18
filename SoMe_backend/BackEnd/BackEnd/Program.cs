256582284916de162566447b40ecf9bab345f303
Jacob Chen Jensen <jacobchenjensen@gmail.com>
2024-03-18 14:41:29 +0100

    users endpoints without dtos

--- SoMe_backend/BackEnd/BackEnd/Program.cs
+++ SoMe_backend/BackEnd/BackEnd/Program.cs
@@ -1,15 +1,28 @@
 using BackEnd.Data;
+using BackEnd.Endpoints;
 using BackEnd.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<SoMeContext>();
 builder.Services.AddScoped<IRepository, Repository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowAnyHeader();
+                      });
+});
+
 var app = builder.Build();
+
+
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,7 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.ConfigureUserEndpoint();
 app.Run();
 
 public partial class Program { }
