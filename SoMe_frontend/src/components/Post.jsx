42616e9a590996cbd8de101fd2512c39bf4216eb
Sini Suihkonen <sahsuihkonen@gmail.com>
2024-03-18 15:20:03 +0200

    comment component added

--- SoMe_frontend/src/components/Post.jsx
+++ SoMe_frontend/src/components/Post.jsx
@@ -1,11 +1,23 @@
 import PropTypes from "prop-types";
+import Comment from "./Comment";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 function Post({ post }) {
+  const context = useContext(AppContext);
+
   return (
     <div>
       <h3>{post.title}</h3>
       <p>{post.content}</p>
       <p>Author: {post.author}</p>
+      <div>
+        {context.comments
+          .filter((x) => parseInt(x.postId) === parseInt(post.id))
+          .map((comment, index) => (
+            <Comment comment={comment} key={index} />
+          ))}
+      </div>
     </div>
   );
 }
