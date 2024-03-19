5bb8820bfcf7b5aa3983bbdb2275bf03839a73ab
Jacob Chen Jensen <jacobchenjensen@gmail.com>
2024-03-19 15:23:56 +0100

    payload constraints

--- SoMe_backend/BackEnd/BackEnd/Endpoints/PostsEndpoint.cs
+++ SoMe_backend/BackEnd/BackEnd/Endpoints/PostsEndpoint.cs
@@ -28,7 +28,8 @@ namespace BackEnd.Endpoints
         public static async Task<IResult> CreatePost(IRepository repository, CreatePostPayload payload)
         {
             User? user = await repository.GetUser(payload.userId);
-            if (user == null) { return TypedResults.NotFound(); }
+            if (user == null) { return TypedResults.NotFound(new {error="User not found."}); }
+            if(payload.title== null || !(payload.title is string)||payload.content == null||!(payload.content is string)) { return TypedResults.BadRequest(new { error = "Title and content requred, and must be in string format." }); }
             Post post = new Post { title = payload.title, content = payload.content, userId=payload.userId, likes = 0 };
             var result = await repository.CreatePost(post, user);
             if (result == null)
@@ -40,13 +41,27 @@ namespace BackEnd.Endpoints
         public static async Task<IResult> GetPost(IRepository repository, int postId)
         {
             var post = await repository.GetPost(postId);
-            if (post == null) { return TypedResults.NotFound(); }
+            if (post == null) { return TypedResults.NotFound(new {error="Post not found."}); }
             return TypedResults.Ok(new PostResponseDTO(post));
         }
         public static async Task<IResult> UpdatePost(IRepository repository, UpdatePostPayload payload, int postId)
         {
             Post? postToUpdate = await repository.GetPost(postId);
-            if (postToUpdate == null) { return TypedResults.BadRequest(); }
+            if (postToUpdate == null) { return TypedResults.NotFound(new {error="Post not found."}); }
+            if(!(payload.title is string))
+            {
+                if(!(payload.title == null))
+                {
+                    return TypedResults.BadRequest(new { error = "Title must be string type." });
+                }
+            }
+            if (!(payload.title is string))
+            {
+                if (!(payload.title == null))
+                {
+                    return TypedResults.BadRequest(new { error = "Content must be string type." });
+                }
+            }
             Post newInfo = new Post { title = payload.title, content=payload.content };
             var result = await repository.UpdatePost(postId, newInfo);
             if (result == null)
@@ -58,9 +73,8 @@ namespace BackEnd.Endpoints
         public static async Task<IResult> DeletePost(IRepository repository, int postId)
         {
             Post? post = await repository.GetPost(postId);
-            if (post == null) { return TypedResults.BadRequest(); }
+            if (post == null) { return TypedResults.NotFound(new {error="Post not found."}); }
             var result = await repository.DeletePost(postId);
-            if (result == null) { return TypedResults.NotFound(); }
             return TypedResults.Ok(new PostResponseDTO(result));
         }
     }
